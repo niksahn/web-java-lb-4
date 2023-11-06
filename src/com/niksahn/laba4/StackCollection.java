2e891d54fe328cdbb87f5e3294f8925291379419
Nikita Sahno <n.sahno@smartway.today>
2023-11-06 17:27:46 +0300

    stack agregation

--- src/com/niksahn/laba4/StackCollection.java
+++ src/com/niksahn/laba4/StackCollection.java
@@ -1,9 +1,36 @@
 package com.niksahn.laba4;
 
-import java.util.Iterator;
+import java.util.Arrays;
 import java.util.Stack;
 
-public class StackCollection extends Stack<Integer> {
+public class StackCollection {
+    private Stack<String> stack = new Stack<>();
 
+    public StackCollection revert() {
+        Stack<String> newStack =  stack;
+        stack = new Stack<>();
+        var newStackIter = newStack.iterator();
+        while (newStackIter.hasNext()) {
+            stack.push(newStack.pop());
+        }
+        return this;
+    }
+
+    @Override
+    public String toString(){
+        String str = "";
+        for (String s : stack) {
+            str += s + " ";
+        }
+        return  str;
+    }
+
+    /**
+     * Добавляет в объект данные из строки
+     * **/
+    public void  addFromString(String str){
+        var strArray = str.split(" ");
+        Arrays.stream(strArray).forEach(this.stack::push);
+    }
 }
 
