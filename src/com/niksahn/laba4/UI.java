5827bd15e778087a22ba13e22a37bc0cbabec69f
Alex <sasha_novosel@mail.ru>
2023-11-06 16:56:16 +0300

    Перенёс из прошлой лабы

--- src/com/niksahn/laba4/UI.java
+++ src/com/niksahn/laba4/UI.java
@@ -1,5 +1,21 @@
 package com.niksahn.laba4;
 
+import java.util.Scanner;
+
 public class UI {
+    static private final Scanner in = new Scanner(System.in);
+
+    static private void displayInfo(String str) {
+        System.out.println(str);
+    }
+
+    static private String inputInfo() {
+        return in.nextLine();
+    }
+
+    static public void displayInfoMsg() {
+        displayInfo("22ВП3 Бригада 3 Сахно и Новосельцев \n Вариант №3: Класс Stack \n");
+    }
+
 
 }
